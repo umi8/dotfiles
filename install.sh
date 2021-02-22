a9880b4b0850a317e931ac39906b139e292ef53e
umi8 <umi8@users.noreply.github.com>
2021-02-22 14:13:27 +0900

    install powerline fonts

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install Powerline fonts
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
