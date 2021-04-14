ff7ce855d71ce49b6655a83107d9e89bbca03fc1
umi8 <umi8@users.noreply.github.com>
2021-04-14 22:00:49 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,53 @@
-# Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# ======================
+# Command Line Tools
+# ======================
+echo "install COmmand Line Tools for Xcode"
+xcode-select --install
 
-# Install Powerline fonts
-# clone
+# ======================
+# homebrew
+# ======================
+echo "install homebrew"
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "run brew doctor"
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brwe update"
+which brew >/dev/null 2>&1 && brew update --verbose
+
+echo "run brew bundle"
+which brew >/dev/null 2>&1 && brew bundle --file preferences/homebrew/Brewfile
+
+# ======================
+# Karabiner-Elements
+# ======================
+cp -p preferences/karabiner/karabiner.json ~/.config/karabiner/
+
+# ======================
+# font
+# ======================
+
+echo "clone Powerline fonts"
 git clone https://github.com/powerline/fonts.git --depth=1
-# install
+
+echo "install fonts"
 cd fonts
 ./install.sh
-# clean-up a bit
+
+echo "clean-up"
 cd ..
 rm -rf fonts
 
+# ======================
+# link
+# ======================
+
+echo "create symbolic link"
+
+echo "link to .zshrc"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+echo "link to .vimrc"
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+echo "link to .ideavimrc"
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
