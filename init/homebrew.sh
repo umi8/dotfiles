c1bc66249b36548e82fee7676e721280ff97dec9
Hiromi Furukawa <umi8@users.noreply.github.com>
2023-05-26 18:11:34 +0900

    feat: merge homebrew installation and packages installation

--- init/homebrew.sh
+++ init/homebrew.sh
@@ -13,9 +13,21 @@ if ! command -v brew >/dev/null 2>&1; then
   exit 1
 fi
 
+echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Check for problems with Homebrew settings and dependencies
 brew doctor
 # Update Homebrew repository
 brew update --verbose
+
+BREWFILE=~/dotfiles/preferences/homebrew/Brewfile
+if [ ! -f "${BREWFILE}" ]; then
+    echo "Brewfile does not exist."
+    exit 1
+fi
+
+# Install packages
+brew bundle --file "${BREWFILE}"
 
 echo "Homebrew installation completed."
