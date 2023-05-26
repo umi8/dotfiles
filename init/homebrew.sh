4d7c383830123aa81088b00d376941a25389db83
Hiromi Furukawa <umi8@users.noreply.github.com>
2023-05-26 18:14:38 +0900

    feat: add Homebrew to PATH

--- init/homebrew.sh
+++ init/homebrew.sh
@@ -7,14 +7,13 @@ fi
 
 echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 if ! command -v brew >/dev/null 2>&1; then
   echo "Homebrew installation failed."
   exit 1
 fi
-
-echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Check for problems with Homebrew settings and dependencies
 brew doctor
